20d5c65809ad1d5bf8460a00f972b1f76c77d29c
nesv005 <nesv005@gmail.com>
2022-04-15 18:11:04 +0700

    Implement viewmodel survey

--- app/src/main/java/com/example/testcompose/survey/SurveyViewModel.kt
+++ app/src/main/java/com/example/testcompose/survey/SurveyViewModel.kt
@@ -4,11 +4,10 @@ import android.net.Uri
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 const val simpleDateFormatPattern = "EEE, MMM d"
 
@@ -49,4 +48,104 @@ class SurveyViewModel(
             _uiState.value = surveyInitialState
         }
     }
+
+    fun computeResult(surveyQuestions: SurveyState.Questions) {
+        val answers = surveyQuestions.questionsState.mapNotNull { it.answer }
+        val result = surveyRepository.getSurveyResult(answers)
+        _uiState.value = SurveyState.Result(surveyQuestions.surveyTitle, result)
+    }
+
+    fun onDatePicked(questionID: Int, pickerSelection: Long?) {
+        val selectedDate = Date().apply {
+            time = pickerSelection ?: getCurrentDate(questionID)
+        }
+        val formattedDate =
+            SimpleDateFormat(simpleDateFormatPattern, Locale.getDefault()).format(selectedDate)
+        updateStateWithActionResult(questionID, SurveyActionResult.Date(formattedDate))
+    }
+
+    fun getCurrentDate(questionID: Int) : Long {
+        return getSelectedDate(questionID)
+    }
+
+    fun getUriToSaveImage(): Uri? {
+        uri = photoUriManager.buildNewUri()
+        return uri
+    }
+
+    fun onImageSaved() {
+        uri?.let { uri ->
+            getLatestQuestionId()?.let { questionId ->
+                updateStateWithActionResult(questionId, SurveyActionResult.Photo(uri))
+            }
+        }
+    }
+
+    // TODO: Ideally this should be stored in the database
+    fun doNotAskForPermission() {
+        askForPermissions = false
+    }
+
+    private fun updateStateWithActionResult(questionID: Int, result: SurveyActionResult) {
+        val latestState = _uiState.value
+        if (latestState != null && latestState is SurveyState.Questions) {
+            val question = latestState.questionsState.first { questionState ->
+                questionState.question.id == questionID
+            }
+            question.answer = Answer.Action(result)
+            question.enableNext = true
+        }
+    }
+
+    private fun getLatestQuestionId(): Int? {
+        val latestState = _uiState.value
+        if (latestState != null && latestState is SurveyState.Questions) {
+            return latestState.questionsState[latestState.currentQuestionIndex].question.id
+        }
+        return null
+    }
+
+    private fun getSelectedDate(questionID: Int): Long {
+        val latestState = _uiState.value
+        var ret = Date().time
+        if (latestState != null && latestState is SurveyState.Questions) {
+            val question =
+                latestState.questionsState.first { questionState ->
+                    questionState.question.id == questionID
+                }
+            val answer: Answer.Action? = question.answer as Answer.Action?
+            if (answer != null && answer.result is SurveyActionResult.Date) {
+                val formatter = SimpleDateFormat(simpleDateFormatPattern, Locale.ENGLISH)
+                val formatted = formatter.parse(answer.result.date)
+                if (formatted is Date)
+                    ret = formatted.time
+            }
+        }
+        return ret
+    }
 }
+
+class SurveyViewModelFactory(
+    private val photoUriManager: PhotoUriManager
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SurveyViewModel::class.java)) {
+            return SurveyViewModel(SurveyRepository, photoUriManager) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
